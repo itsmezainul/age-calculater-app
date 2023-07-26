3cb3669a638a8af976855502c55de5cc37b79966
itsmezainul <iamzainul@outlook.com>
2023-07-26 16:37:08 +0530

    form element and error area

--- main.js
+++ main.js
@@ -10,37 +10,5 @@ const form = document.querySelector("form");
 let monthWiseDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-//   if (!inputDay.value || !inputMonth.value || !inputYear.value) {
-//     console.log("this field is required");
-//   } else if (inputDay.value > monthWiseDays[inputMonth.value + 1]) {
-//     console.log("Must be a valid day");
-//   } else if (inputMonth.value > monthWiseDays.length) {
-//     console.log("Must be a valid month");
-//   } else if (inputYear.value > year) {
-//     console.log("Must be a valid year");
-//   } else if (
-//     inputYear === year &&
-//     inputMonth.value === month &&
-//     inputDay.value > day
-//   ) {
-//     console.log("Must be a valid all");
-//   } else if (inputYear === year && inputMonth.value > month) {
-//     console.log("Must be a valid all");
-//   }
 
 });
-
-
-function checkError(day,month,year) {
-
-}
-
-function leapYearCheck(year) {
-  year.value % 4 !== 0
-    ? console.log("Not Leap Year")
-    : year.value % 100 === 0 && year.value % 400 !== 0
-    ? console.log("Not Leap Year")
-    : console.log("Leap Year");
-}
-
